7fa35b32de71d75f541dd6b926c638ea1254d7d3
viniciusraja <viniciusrfl@hotmail.com>
2022-03-27 13:34:11 -0300

    chore: config typescript

--- index.native.ts
+++ index.native.ts
@@ -1,7 +1,7 @@
 /**
  * @format
  */
-
+import 'react-native/tvos-types.d';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import MovieCard from './components/PictureCard';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View
+          style={{
+            backgroundColor: 'red',
+          }}></View>
+        <MovieCard />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
